23c7547a1045a805cac824cea5c2447dddaaaf78
Maeliosa Leamy <Maeliosa.Leamy@ibm.com>
2022-04-18 15:25:24 +0100

    Note

--- app/src/main/java/org/wit/m/NoteActivity.kt
+++ app/src/main/java/org/wit/m/NoteActivity.kt
@@ -7,7 +7,5 @@ class NoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note)
-
-
     }
 }
